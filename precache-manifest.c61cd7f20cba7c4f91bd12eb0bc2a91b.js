84d386564af30c9e4fdd57a0f2774328577f9cef
poskachei <vposkachey@gmail.com>
2020-08-02 14:21:35 +0300

    Updates

---
+++ precache-manifest.c61cd7f20cba7c4f91bd12eb0bc2a91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadf8f8bc04070b98df47e1d9a7bd920",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1534fc97abe31b3f6404",
+    "url": "/goit-react-hw-01-components/static/css/main.acc007bd.chunk.css"
+  },
+  {
+    "revision": "2d7c8c094cd3b280d669",
+    "url": "/goit-react-hw-01-components/static/js/2.c819031c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.c819031c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1534fc97abe31b3f6404",
+    "url": "/goit-react-hw-01-components/static/js/main.d5e57b3c.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
